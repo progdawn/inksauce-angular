cc98b1989367d1468676c42af61798f8734c89e5
Dawn Myers <progdawn@gmail.com>
2017-12-13 16:24:22 -0600

    Made components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BrandsComponent } from './brands/brands.component';
+import { GearComponent } from './gear/gear.component';
+import { AbilitiesComponent } from './abilities/abilities.component';
+import { WeaponsComponent } from './weapons/weapons.component';
+import { SubsComponent } from './subs/subs.component';
+import { SpecialsComponent } from './specials/specials.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { SubmitComponent } from './submit/submit.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BrandsComponent,
+    GearComponent,
+    AbilitiesComponent,
+    WeaponsComponent,
+    SubsComponent,
+    SpecialsComponent,
+    GalleryComponent,
+    SubmitComponent
   ],
   imports: [
     BrowserModule
