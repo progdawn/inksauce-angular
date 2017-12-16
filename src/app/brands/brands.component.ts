b0c3b1e849d18f654a629121d98392f759558f7e
Dawn Myers <progdawn@gmail.com>
2017-12-16 17:17:15 -0600

    Brands work
    Woot woot! Using *ngfor to grab the data from a json file. Since I figured this out, I might get a date tonight after all!

--- src/app/brands/brands.component.ts
+++ src/app/brands/brands.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {LoggingService} from '../logging.service';
+import {DataService} from '../data.service';
 
 @Component({
   selector: 'app-brands',
   templateUrl: './brands.component.html',
-  styleUrls: ['./brands.component.css']
+  styleUrls: ['./brands.component.css'],
+  providers: [DataService]
 })
 export class BrandsComponent implements OnInit {
 
-  constructor() { }
+  brands:any = [];
+
+  constructor(private logger:LoggingService, private dataService: DataService) { }
+
+  logIt(){
+    this.logger.log();
+  }
 
   ngOnInit() {
+    this.dataService.fetchBrands().subscribe(
+      (data) => this.brands = data
+    );
   }
 
 }
