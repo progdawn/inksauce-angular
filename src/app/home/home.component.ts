a3074e227fd62f24625f4dde72aac9194f044f3d
Dawn Myers <progdawn@gmail.com>
2017-12-16 12:28:05 -0600

    Fixed carousel properly
    Fixes #1. The carousel is now written using ng-bootstrap, instead of all that html bootstrap stuff. I was able to ditch the css that goes along with it, and of course the javascript. Learned how to set the interval with typescript.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {LoggingService} from '../logging.service';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap/carousel/carousel-config';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +11,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private logger:LoggingService, config: NgbCarouselConfig) {
+    config.interval = 3000;
+  }
+
+  logIt(){
+    this.logger.log();
+  }
 
   ngOnInit() {
   }
