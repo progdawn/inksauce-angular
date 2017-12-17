9619419722add3e46a421362a1c6fad8e135fb26
Dawn Myers <progdawn@gmail.com>
2017-12-17 14:33:02 -0600

    Tabs show up
    Now to get the data showing 🤔

--- src/app/gear/gear.component.ts
+++ src/app/gear/gear.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from '../data.service';
 
 @Component({
   selector: 'app-gear',
   templateUrl: './gear.component.html',
-  styleUrls: ['./gear.component.css']
+  styleUrls: ['./gear.component.css'],
+  providers: [DataService]
 })
 export class GearComponent implements OnInit {
 
-  constructor() { }
+  gears:any = [];
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.dataService.fetchGear().subscribe(
+      (data) => this.gears = data
+    );
   }
 
 }
